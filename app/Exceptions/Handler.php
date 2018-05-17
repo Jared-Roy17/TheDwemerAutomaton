3fb182b38171d52a12bbabbf248568bf7a0e828f
Woeler <woeler@esoleaderboards.com>
2018-05-17 15:56:21 +0200

    Add exception reporter

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -21,6 +21,8 @@ class Handler extends ExceptionHandler
         HttpException::class,
         ModelNotFoundException::class,
         ValidationException::class,
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Session\TokenMismatchException::class,
     ];
 
     /**
@@ -29,9 +31,32 @@ class Handler extends ExceptionHandler
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
      * @param \Exception $e
+     *
+     * @throws Exception
      */
     public function report(Exception $e)
     {
+        if ('production' === env('APP_ENV')) {
+            if ($this->shouldReport($e)) {
+                $str = '----- '.date('Y-m-d H:i:s').' '.env('DEFAULT_TIMEZONE').' -----'.PHP_EOL;
+                $str .= 'New `'.get_class($e).'`'.PHP_EOL;
+                $str .= '```'.$e->getMessage().'```'.PHP_EOL;
+                $str .= '`In '.$e->getFile().' on line '.$e->getLine().'.`';
+                $ch = curl_init(env('DEBUG_HOOK'));
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(
+                    [
+                        'content'    => $str,
+                        'username'   => env('BOT_USERNAME'),
+                        'avatar_url' => env('BOT_AVATAR'),
+                    ]
+                ));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_exec($ch);
+                curl_close($ch);
+            }
+        }
+
         parent::report($e);
     }
 
